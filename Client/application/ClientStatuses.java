7d281d8697078b0ccd7ff00b7c5f9dfd89de7293
Mateusz <mlodyajdza@interia.pl>
2017-08-06 21:55:43 +0200

    Clean code

--- Client/application/ClientStatuses.java
+++ Client/application/ClientStatuses.java
@@ -2,6 +2,7 @@ package application;
 
 public enum ClientStatuses {
 	CLIENT_TITLE("CLIENT"),
+	CONNECTION_THREAD("CONNECTION_WITH_SERVER_THREAD"),
 	CLIENT_NOT_CONNECTED("Not connected"),
 	CLIENT_CONNECTED("Connected"),
 	CLIENT_AUTHORIZED("Authorized"),
@@ -11,8 +12,7 @@ public enum ClientStatuses {
 	OK("OK"),
 	NONE("NONE"),
 	NO("NO"),
-	WRONG_AUTH_FIELD_VALUE("Type authorization code"),
-	CONNECTION_THREAD("CONNECTION_WITH_SERVER_THREAD");
+	WRONG_AUTH_FIELD_VALUE("Type authorization code");
 	
 	private String statusText;
 	
